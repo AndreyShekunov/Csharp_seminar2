c5c03cd9013d1aaac6025eeed6ce652d005e16b5
Шекунов Андрей <ashekunov@gmail.com>
2022-09-06 11:56:46 +0500

    Задача 9 Показывает наибольшую цифру случайного числа

---
+++ task9/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 9. Напишите программу, которая выводит случайное число
+// из отрезка [10, 99] и показывает наибольшую цифру числа.
+// Например:
+// 78 -> 8
+// 12-> 2
+// 85 -> 8
+
+Random rand = new Random();
+int number = rand.Next(10, 100);
+Console.WriteLine(number);
+
+int digital2 = number % 10;
+int digital1 = number / 10;
+
+if(digital1 > digital2)
+{
+    Console.WriteLine(digital1);
+}
+else
+{
+    Console.WriteLine(digital2);
+}
